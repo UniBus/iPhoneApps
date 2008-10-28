0c142e925cb1743009ab661f8abaf072c5ecb223
Zhen <zhen@unibus.ca>
2008-10-28 17:27:33 +0000

    clean up the code.
    git-svn-id: http://zyao.unfuddle.com/svn/zyao_iphoneapps/trunk@143 8c287f6f-53b1-4db0-8e59-0c5774308c12

---
+++ DataProcess/BusRoute.h
@@ -0,0 +1,10 @@
+//
+//  BusRoute.h
+//  DataProcess
+//
+//  Created by Zhenwang Yao on 28/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+int convertRoutesToSQLite(NSString *routeFile, NSString *dbName);
+
--- DataProcess/DataProcess.m
+++ DataProcess/DataProcess.m
@@ -1,33 +1,67 @@
 #import <Foundation/Foundation.h>
-#import "StopQuery.h"
-#import "StopQuery-CSV.h"
+#import "BusStop.h"
+#import "BusRoute.h"
+#import "Favorite.h"
 
 int main (int argc, const char * argv[]) 
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	printf("\nGoogle Transit Feed data convertor.\n");
-	printf("Covert stops.txt from csv format to sqlite format.\n");
+	printf("Covert GTFS data from csv format to sqlite format.\n");
 	printf("Copyright @ Zhenwang.Yao 2008\n");
 	
-	if (argc != 3)
+	BOOL parameterError = NO;
+	if (argc >= 2)
+	{	
+		NSString *action = [NSString stringWithUTF8String:argv[1]];		
+		if ([action compare:@"-addroute" options:NSCaseInsensitiveSearch]==NSOrderedSame)
+		{
+			if (argc == 4)
+			{				
+				printf("\n Converting routes %s to %s\n\n", argv[2], argv[3]);
+				NSString *srcFile = [NSString stringWithUTF8String:argv[2]];
+				NSString *sqlFile = [NSString stringWithUTF8String:argv[3]];
+	
+				convertRoutesToSQLite(srcFile, sqlFile);
+			}
+			else
+				parameterError = YES;
+		}		
+		else if ([action compare:@"-addstop" options:NSCaseInsensitiveSearch]==NSOrderedSame)
+		{		
+			if (argc == 4)
+			{				
+				printf("\n Converting stops %s to %s\n\n", argv[2], argv[3]);
+				NSString *srcFile = [NSString stringWithUTF8String:argv[2]];
+				NSString *sqlFile = [NSString stringWithUTF8String:argv[3]];
+			
+				convertStopsToSQLite(srcFile, sqlFile);
+			}
+			else 
+				parameterError = YES;
+		}		
+		else if ([action compare:@"-addfavorite" options:NSCaseInsensitiveSearch]==NSOrderedSame)
+		{		
+			if (argc == 3)
+			{
+				printf("\n Add favorite table into %s\n\n", argv[2]);			
+				NSString *sqlFile = [NSString stringWithUTF8String:argv[2]];
+				addFavoriteTable(sqlFile);
+			}
+			else 
+				parameterError = YES;
+		}
+		else
+			parameterError = YES;
+	}
+
+	if (parameterError)
 	{
 		printf("Usage:\n");
-		printf("\t DataProcess [stops.csv stops.sqlite]\n\n");
+		printf("\t DataProcess -[action] [src.csv db.sqlite]\n\n");
 	}
-	else
-	{	
-		printf("\n Converting %s to %sn", argv[1], argv[2]);
-		NSString *srcFile = [NSString stringWithUTF8String:argv[1]];
-		NSString *sqlFile = [NSString stringWithUTF8String:argv[2]];
 	
-		StopQuery_CSV *stopQuery = [StopQuery_CSV initWithFile:srcFile];
-		if (stopQuery)
-		{
-			[stopQuery saveToSQLiteDatabase:sqlFile];
-		}
-		[stopQuery release];
-	}
 	
     [pool drain];
     return 0;
