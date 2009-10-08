48ed06deb273aa313b4bfd91cf49632b0a85575a
Zhen <zhen@unibus.ca>
2009-10-08 17:33:26 +0000

    delete iBus-Fixed, restarted from iBus-Universal just to see the diference.
    git-svn-id: http://zyao.unfuddle.com/svn/zyao_iphoneapps/trunk@313 8c287f6f-53b1-4db0-8e59-0c5774308c12

--- iBus-Fixed/Classes/Compression.h
+++
@@ -1,9 +0,0 @@
-//
-//  Compression.h
-//  iBus-Universal
-//
-//  Created by Zhenwang Yao on 27/09/09.
-//  Copyright 2009 Zhenwang Yao. All rights reserved.
-//
-
-BOOL UnzipFile(NSString *sourcePath, NSString *destPath);
